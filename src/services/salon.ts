e0e0818de9e8fbbf479a7981e47e02f074b87977
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-11-26 14:27:44 +0530

    unwanted files removed

--- src/services/salon.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = "https://express-mongo-connection-sigma.vercel.app/api/salons";
-
-export const getAllSalons = async () => {
-  const response = await axios.get(API_URL);
-  console.log(response);
-  return response.data.salons;
-};
--- src/services/service.ts
+++
@@ -1,7 +0,0 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-
-export async function getAllServices() {
-  const res = await fetch(`${BASE_URL}/services`, { cache: "no-store" });
-  if (!res.ok) throw new Error("Failed to fetch services");
-  return res.json();
-}
